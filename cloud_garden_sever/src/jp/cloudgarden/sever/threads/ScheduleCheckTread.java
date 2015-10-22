b2b8711f87af93a7fec39275df6e0c257e9875ea
Takenouchi Keita <t-keita@ist.osaka-u.ac.jp>
2015-10-23 04:57:00 +0900

    add ShceduleCheckTread.java

---
+++ cloud_garden_sever/src/jp/cloudgarden/sever/threads/ScheduleCheckTread.java
@@ -0,0 +1,33 @@
+package jp.cloudgarden.sever.threads;
+
+import jp.cloudgarden.sever.jax.CloudController;
+
+public class ScheduleCheckTread extends Thread {
+	private CloudController controller;
+	private boolean isRunning = false;
+	private int intervalSec = 60;
+
+	public ScheduleCheckTread(CloudController controller) {
+		this.controller = controller;
+	}
+
+	@Override
+	public void run() {
+		isRunning = true;
+
+		while (isRunning) {
+			controller.checkSchedules();
+
+			try {
+				Thread.sleep(intervalSec * 1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public void stopThread(){
+		this.isRunning = false;
+	}
+
+}
